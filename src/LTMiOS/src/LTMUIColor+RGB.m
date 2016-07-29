17adf5468f4cba3449a9b50b0d3064f249e8986b
wangxiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-07-29 20:04:59 +0800

    修改代码格式

--- src/LTMiOS/src/LTMUIColor+RGB.m
+++ src/LTMiOS/src/LTMUIColor+RGB.m
@@ -14,14 +14,12 @@ static const uint32_t kColorMaskGreen   = 0xFF0000;
 static const uint32_t kColorMaskBlue    = 0xFF00;
 static const uint32_t kColorMaskAlpha   = 0xFF;
 
-UIColor *LTMUIColorHRGB(uint32_t rgbValue)
-{
+UIColor *LTMUIColorHRGB(uint32_t rgbValue) {
     uint32_t newValue = (rgbValue << 8) | kColorMaskAlpha;
     return LTMUIColorHRGBA(newValue);
 }
 
-UIColor *LTMUIColorHRGBA(uint32_t rgbaValue)
-{
+UIColor *LTMUIColorHRGBA(uint32_t rgbaValue) {
     //  R   G  B  A
     //0xFF FF FF FF
     uint8_t r = (rgbaValue & kColorMaskRed) >> 24;
@@ -31,16 +29,14 @@ UIColor *LTMUIColorHRGBA(uint32_t rgbaValue)
     return LTMUIColorRGBA(r,g,b,a);
 }
 
-UIColor *LTMUIColorRGBA(uint8_t r, uint8_t g, uint8_t b, CGFloat a)
-{
+UIColor *LTMUIColorRGBA(uint8_t r, uint8_t g, uint8_t b, CGFloat a) {
     return [UIColor colorWithRed:(r / kLTMRGBMax)
                            green:(g / kLTMRGBMax)
                             blue:(b / kLTMRGBMax)
                            alpha:a];
 }
 
-UIColor *LTMUIColorRGB(uint8_t r, uint8_t g, uint8_t b)
-{
+UIColor *LTMUIColorRGB(uint8_t r, uint8_t g, uint8_t b) {
     return LTMUIColorRGBA(r, g, b, 1.0f);
 }
 
