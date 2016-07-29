4073f3ed57d1e536dff2251c79e06c3b098416a3
wangxiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-07-29 19:00:52 +0800

    添加OC RGB方法

--- src/LTMiOS/src/LTMUIColor+RGB.h
+++ src/LTMiOS/src/LTMUIColor+RGB.h
@@ -18,5 +18,9 @@ UIKIT_EXTERN UIColor * LTMUIColorRGBA(uint8_t r, uint8_t g, uint8_t b, CGFloat a
 
 @interface UIColor (LTMUIColorRGBAdditions)
 
++ (UIColor *)ltm_colorWithHRGB:(uint32_t)hrgbValue;
++ (UIColor *)ltm_colorWithHRGBA:(uint32_t)hrgbaValue;
 
++ (UIColor *)ltm_colorWithRed:(uint8_t)red green:(uint8_t)green blue:(uint8_t)blue;
++ (UIColor *)ltm_colorWithRed:(uint8_t)red green:(uint8_t)green blue:(uint8_t)blue alpha:(CGFloat)alpha;
 @end
--- src/LTMiOS/src/LTMUIColor+RGB.m
+++ src/LTMiOS/src/LTMUIColor+RGB.m
@@ -8,7 +8,7 @@
 
 #import "LTMUIColor+RGB.h"
 
-static const CGFloat kFBRGBMax = 255.0f;
+static const CGFloat kLTMRGBMax = 255.0f;
 static const uint32_t kColorMaskRed     = 0xFF000000;
 static const uint32_t kColorMaskGreen   = 0xFF0000;
 static const uint32_t kColorMaskBlue    = 0xFF00;
@@ -27,15 +27,15 @@ UIColor *LTMUIColorHRGBA(uint32_t rgbaValue)
     uint8_t r = (rgbaValue & kColorMaskRed) >> 24;
     uint8_t g = (rgbaValue & kColorMaskGreen) >> 16;
     uint8_t b = (rgbaValue & kColorMaskBlue) >> 8;
-    uint8_t a = (rgbaValue & kColorMaskAlpha) / kFBRGBMax;
+    uint8_t a = (rgbaValue & kColorMaskAlpha) / kLTMRGBMax;
     return LTMUIColorRGBA(r,g,b,a);
 }
 
 UIColor *LTMUIColorRGBA(uint8_t r, uint8_t g, uint8_t b, CGFloat a)
 {
-    return [UIColor colorWithRed:(r / kFBRGBMax)
-                           green:(g / kFBRGBMax)
-                            blue:(b / kFBRGBMax)
+    return [UIColor colorWithRed:(r / kLTMRGBMax)
+                           green:(g / kLTMRGBMax)
+                            blue:(b / kLTMRGBMax)
                            alpha:a];
 }
 
@@ -45,5 +45,32 @@ UIColor *LTMUIColorRGB(uint8_t r, uint8_t g, uint8_t b)
 }
 
 @implementation UIColor (LTMUIColorRGBAdditions)
+
+
++ (UIColor *)ltm_colorWithHRGB:(uint32_t)hrgbValue {
+    uint32_t newValue = (hrgbValue << 8) | kColorMaskAlpha;
+    return [self ltm_colorWithHRGBA:newValue];
+}
+
++ (UIColor *)ltm_colorWithHRGBA:(uint32_t) hrgbaValue{
+    uint8_t r = (hrgbaValue & kColorMaskRed) >> 24;
+    uint8_t g = (hrgbaValue & kColorMaskGreen) >> 16;
+    uint8_t b = (hrgbaValue & kColorMaskBlue) >> 8;
+    uint8_t a = (hrgbaValue & kColorMaskAlpha) / kLTMRGBMax;
+    UIColor *color = [self ltm_colorWithRed:r green:g blue:b alpha:a];
+    return color;
+}
+
+
++ (UIColor *)ltm_colorWithRed:(uint8_t)red green:(uint8_t)green blue:(uint8_t)blue {
+    return [self ltm_colorWithRed:red green:green blue:blue alpha:1.f];
+}
+
++ (UIColor *)ltm_colorWithRed:(uint8_t)red green:(uint8_t)green blue:(uint8_t)blue alpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:(red   / kLTMRGBMax)
+                           green:(green / kLTMRGBMax)
+                            blue:(blue  / kLTMRGBMax)
+                           alpha:alpha];
+}
 
 @end
